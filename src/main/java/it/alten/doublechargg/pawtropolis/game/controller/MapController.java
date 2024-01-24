674764c00390e8e921bf6950ff83b994a20852cd
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2024-01-24 15:32:23 +0100

    Add method to set locked room and add method to insert keyObjects in rooms

--- src/main/java/it/alten/doublechargg/pawtropolis/game/controller/MapController.java
+++ src/main/java/it/alten/doublechargg/pawtropolis/game/controller/MapController.java
@@ -17,12 +17,7 @@ public class MapController {
     private static final Random RANDOMIZER = new Random();
     private final List<Room> roomList;
     private final RoomFactory roomFactory;
-    private final Item[] keys = {
-            new Item(4L, "Penny", "a penny", 1),
-            new Item(5L, "Key", "a key", 1),
-            new Item(6L, "Bomb", "a bomb", 3),
-            new Item(7L, "Golden Key", "a golden key", 10)
-    };
+    private final List<Integer> lockedRoomIndexes;
     private final Map<Item, Room> lockedRoomMap;
 
     @Autowired
@@ -30,10 +25,11 @@ public class MapController {
         this.roomFactory = roomFactory;
         roomList = new ArrayList<>();
         lockedRoomMap = new HashMap<>();
+        lockedRoomIndexes = new ArrayList<>();
     }
 
     public void createMap() {
-        val MINIMUM_ROOMS = 1;
+        val MINIMUM_ROOMS = 4;
         val MAXIMUM_ROOMS = 8;
         final int roomNumber = RANDOMIZER.nextInt(MINIMUM_ROOMS, MAXIMUM_ROOMS + 1);
         for (int i = 0; i < roomNumber; i++) {
@@ -49,23 +45,46 @@ public class MapController {
             }
             connectRooms(CardinalPoints.values()[selectedCardinalPointIndex], roomList.get(i), roomList.get(i + 1));
         }
+        setLockedRooms(roomNumber);
+        addKeyObjectsInRooms();
 
-        List<Integer> lockedRoomIndexes = new ArrayList<>();
-        val MAXIMUM_LOCKED_ROOMS = 4;
-        final int lockedRoomNumber = RANDOMIZER.nextInt(MAXIMUM_LOCKED_ROOMS);
-        for (int i = 0; i < lockedRoomNumber; i++) {
-            int lockedRoomIndex = RANDOMIZER.nextInt(roomNumber);
-            lockedRoomIndexes.add(lockedRoomIndex);
-            roomList.get(lockedRoomIndex).setLockedDoor(true);
-            int keyRoomIndex = RANDOMIZER.nextInt(lockedRoomIndex);
-            roomList.get(keyRoomIndex).addItem(keys[i]);
-            lockedRoomMap.put(keys[i], roomList.get(lockedRoomIndex));
-        }
     }
 
     public void connectRooms(CardinalPoints cardinalPoint, Room room1, Room room2) {
         room1.addAdjacentRoom(cardinalPoint, room2);
         room2.addAdjacentRoom(CardinalPoints.getOppositeCardinalPoint(cardinalPoint), room1);
     }
+
+    public void setLockedRooms(int roomNumber){
+        val MAXIMUM_LOCKED_ROOMS = 4;
+        final int lockedRoomNumber = RANDOMIZER.nextInt(1,MAXIMUM_LOCKED_ROOMS);
+        for (int i = 0; i < lockedRoomNumber; i++) {
+            int lockedRoomIndex = RANDOMIZER.nextInt(1, roomNumber);
+            lockedRoomIndexes.add(lockedRoomIndex);
+            roomList.get(lockedRoomIndex).setLockedDoor(true);
+        }
+    }
+
+    public void addKeyObjectsInRooms(){
+        Item[] keyObjects = {
+                new Item(4L, "Penny", "a penny", 1),
+                new Item(5L, "Key", "a key", 1),
+                new Item(6L, "Bomb", "a bomb", 3),
+                new Item(7L, "Picket", "a picket", 10)
+        };
+
+        for(int i = 0; i < lockedRoomIndexes.size(); i ++){
+            int currentKeyObjectRoomIndex = RANDOMIZER.nextInt(lockedRoomIndexes.get(i));
+            Item currentRandomizedKeyObject = keyObjects[RANDOMIZER.nextInt(keyObjects.length)];
+            roomList.get(currentKeyObjectRoomIndex).addItem(currentRandomizedKeyObject);
+            lockedRoomMap.put(currentRandomizedKeyObject, roomList.get(lockedRoomIndexes.get(i)));
+        }
+    }
+
+    public Room getRoomFromItem(Item item){
+        return lockedRoomMap.get(item);
+    }
+
+
 }
 
