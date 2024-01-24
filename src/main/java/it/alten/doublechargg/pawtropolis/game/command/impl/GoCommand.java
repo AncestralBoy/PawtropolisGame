8a338e3b7f5943e98ec15951b10ad5762b5e740f
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2024-01-24 15:35:19 +0100

    Update execute method in GoCommand class, to handle locked door

--- src/main/java/it/alten/doublechargg/pawtropolis/game/command/impl/GoCommand.java
+++ src/main/java/it/alten/doublechargg/pawtropolis/game/command/impl/GoCommand.java
@@ -2,22 +2,28 @@ package it.alten.doublechargg.pawtropolis.game.command.impl;
 
 import it.alten.doublechargg.pawtropolis.game.command.interfaces.CommandWithParam;
 import it.alten.doublechargg.pawtropolis.game.controller.GameController;
+import it.alten.doublechargg.pawtropolis.game.controller.MapController;
 import it.alten.doublechargg.pawtropolis.game.enums.CardinalPoints;
+import it.alten.doublechargg.pawtropolis.game.model.Item;
 import it.alten.doublechargg.pawtropolis.game.model.Player;
 import it.alten.doublechargg.pawtropolis.game.model.Room;
 
 import java.util.Objects;
+import java.util.Scanner;
+
 public class GoCommand implements CommandWithParam {
 
-    private GameController gameController;
-    private Player player;
-    private Room currentRoom;
+    private final GameController gameController;
+    private final Player player;
+    private final Room currentRoom;
+    private final MapController mapController;
+    private static final Scanner scanner = new Scanner(System.in);
 
     public GoCommand(GameController gameController) {
         this.gameController = gameController;
+        mapController = gameController.getMapController();
         player = gameController.getPlayer();
         currentRoom = gameController.getCurrentRoom();
-
     }
 
     @Override
@@ -27,10 +33,42 @@ public class GoCommand implements CommandWithParam {
             return "Not valid input";
         }
         if (currentRoom.adjacentRoomExists(cardinalPoint)) {
-            gameController.setCurrentRoom(currentRoom.getAdjacentRoomByCardinalPoint(cardinalPoint));
-            return String.format("%s entered the room %s%n", player.getName(), gameController.getCurrentRoom().toString());
+            if(currentRoom.getAdjacentRoomByCardinalPoint(cardinalPoint).isLockedDoor()){
+                System.out.println("The door is locked: would you like to use an item to unlock it?");
+                String answer = scanner.nextLine();
+                switch (answer.toLowerCase()){
+                    case "y":
+                        System.out.println("Type the name of the chosen item");
+                        String itemName = scanner.nextLine();
+                        Item item =  player.getItemFromBag(itemName);
+                        if(item != null ){
+                            if(mapController.getRoomFromItem(item) != null){
+                                if(mapController.getRoomFromItem(item).equals(currentRoom.getAdjacentRoomByCardinalPoint(cardinalPoint))){
+                                    currentRoom.getAdjacentRoomByCardinalPoint(cardinalPoint).setLockedDoor(false);
+                                    System.out.println("You unlocked the door!");
+                                    player.removeItem(item);
+                                    gameController.setCurrentRoom(currentRoom.getAdjacentRoomByCardinalPoint(cardinalPoint));
+                                    return String.format("%s entered the room %s%n", player.getName(), gameController.getCurrentRoom().toString());
+                                }
+                            }
+                            else{
+                                return "This is not the right item";
+                            }
+                        }
+                        else{
+                            return "You don't have this Item";
+                        }
+                    case "n":
+                        return null;
+                    default:
+                        return "Incorrect choice";
+                }
+            }
+            else{
+                gameController.setCurrentRoom(currentRoom.getAdjacentRoomByCardinalPoint(cardinalPoint));
+                return String.format("%s entered the room %s%n", player.getName(), gameController.getCurrentRoom().toString());
+            }
         }
         return "Not existent room";
-
     }
 }
